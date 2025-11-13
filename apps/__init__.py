9871f8168d2af863c841e97f5273f8c77e454d3f
ktsmta <220763128+ktsmta@users.noreply.github.com>
2025-11-13 10:02:26 +0900

    add CSRF protection

--- apps/__init__.py
+++ apps/__init__.py
@@ -2,23 +2,39 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 migrate = Migrate()
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
+    # DB設定
     db_path = os.path.join(app.instance_path, "local.db")
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # CSRF用シークレットキー
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["WTF_CSRF_SECRET_KEY"] = os.getenv("WTF_CSRF_SECRET_KEY")
+
+    # 各拡張機能を初期化
     db.init_app(app)
     migrate.init_app(app, db)
+    csrf.init_app(app)
     
+    # モデル読み込み
     from apps.models import category, thread, post, wiki
 
+    # Blueprint登録
+    # core
     from apps.core import core_bp
     app.register_blueprint(core_bp, url_prefix="/")
+
+    # admin
+    from apps.admin import admin_bp
+    app.register_blueprint(admin_bp, url_prefix='/admin')
 
     return app
