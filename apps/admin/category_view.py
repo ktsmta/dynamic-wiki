0aafdf7e18353258fbf687f504eecc78c7106218
ktsmta <220763128+ktsmta@users.noreply.github.com>
2025-11-19 11:46:52 +0900

    exclude wiki column on the admin-page

--- apps/admin/category_view.py
+++ apps/admin/category_view.py
@@ -1,4 +1,4 @@
 from flask_admin.contrib.sqla import ModelView
 
 class CategoryAdmin(ModelView):
-    form_excluded_columns = ('children', 'threads', 'created_at', 'updated_at')
+    form_excluded_columns = ('children', 'wiki', 'threads', 'created_at', 'updated_at')
