dbf5b19028e79647df6eb1303dc270ceda567c20
ktsmta <220763128+ktsmta@users.noreply.github.com>
2025-11-20 11:19:37 +0900

    change something

--- apps/core/routes/main.py
+++ apps/core/routes/main.py
@@ -1,9 +1,12 @@
 from flask import render_template
 from apps.core import core_bp
 
+from apps.models.category import Category
+
 @core_bp.route("/")
 def index():
-    return render_template("core/index.html")
+    categories = Category.query.all()
+    return render_template("core/index.html", categories=categories)
 
 @core_bp.route("/robots.txt")
 def robots_txt():
